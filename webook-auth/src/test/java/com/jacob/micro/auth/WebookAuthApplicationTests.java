f9a634ae85965cbd0b73d52f95a51f9629432eb8
jdw-art <jdw17733356203@icloud.com>
2024-06-13 22:09:53 +0800

    build(webook-auth):webook authorization test class

---
+++ webook-auth/src/test/java/com/jacob/micro/auth/WebookAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob.micro.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebookAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
