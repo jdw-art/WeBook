a23016e4adb2622473d828b216b63e13d3ee4a21
jdw-art <jdw17733356203@icloud.com>
2024-06-13 22:09:43 +0800

    build(webook-auth):webook authorization startup class

---
+++ webook-auth/src/main/java/com/jacob/micro/auth/WebookAuthApplication.java
@@ -0,0 +1,13 @@
+package com.jacob.micro.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebookAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebookAuthApplication.class, args);
+    }
+
+}
