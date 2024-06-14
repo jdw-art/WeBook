a23bd47928b0ac31ae3eefef5b55a25b92cc6c45
jdw-art <jdw17733356203@icloud.com>
2024-06-14 14:55:33 +0800

    feat(webook-auth):MBG generated code

---
+++ webook-auth/src/main/java/com/jacob/micro/auth/domain/mapper/UserDOMapper.java
@@ -0,0 +1,17 @@
+package com.jacob.micro.auth.domain.mapper;
+
+import com.jacob.micro.auth.domain.dataobject.UserDO;
+
+public interface UserDOMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UserDO record);
+
+    int insertSelective(UserDO record);
+
+    UserDO selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(UserDO record);
+
+    int updateByPrimaryKey(UserDO record);
+}
