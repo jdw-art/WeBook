c96af40166c4af36969aad3a0557003b5d3a606d
jdw-art <jdw17733356203@icloud.com>
2024-06-14 14:54:07 +0800

    build(webook-auth):Add MapperScan annotation

--- webook-auth/src/main/java/com/jacob/micro/auth/WebookAuthApplication.java
+++ webook-auth/src/main/java/com/jacob/micro/auth/WebookAuthApplication.java
@@ -1,9 +1,11 @@
 package com.jacob.micro.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.jacob.micro.auth.domain.mapper")
 public class WebookAuthApplication {
 
     public static void main(String[] args) {
