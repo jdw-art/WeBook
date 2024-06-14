6c776000f2d8ab07086038d04967afdb87af51e8
jdw-art <jdw17733356203@icloud.com>
2024-06-14 11:12:32 +0800

    build(webook-framework-common):Date constant interface

---
+++ webook-framework/webook-common/src/main/java/com/jacob/micro/framework/common/constant/DateConstants.java
@@ -0,0 +1,15 @@
+package com.jacob.micro.framework.common.constant;
+
+/**
+ * @Author: Jacob
+ * @Description: 日期常量接口
+ * @Date: 2024/6/14 10:51
+ * @Version: 1.0
+ */
+public interface DateConstants {
+
+    /**
+     * 年-月-日 时：分：秒
+     */
+    String Y_M_D_H_M_S_FORMAT = "yyyy-MM-dd HH:mm:ss";
+}
