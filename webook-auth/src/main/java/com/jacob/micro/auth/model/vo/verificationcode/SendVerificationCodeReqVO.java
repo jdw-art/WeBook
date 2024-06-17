a64b9b45ca412a5f60047be61fe5951f33eaf8da
jdw-art <jdw17733356203@icloud.com>
2024-06-17 13:36:50 +0800

    feat(webook-auth):SMS verification code input entity class

---
+++ webook-auth/src/main/java/com/jacob/micro/auth/model/vo/verificationcode/SendVerificationCodeReqVO.java
@@ -0,0 +1,23 @@
+package com.jacob.micro.auth.model.vo.verificationcode;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: Jacob
+ * @Description: 短信验证码入参实体类
+ * @Date: 2024/6/16 18:27
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SendVerificationCodeReqVO {
+
+    @NotBlank(message = "手机号不能为空")
+    private String phone;
+}
