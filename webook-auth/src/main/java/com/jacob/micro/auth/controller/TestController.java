1288ebda44a712541b2e1456d8f451abf03bf16d
jdw-art <jdw17733356203@icloud.com>
2024-06-17 13:37:30 +0800

    test(webook-auth):SA-token test interface

--- webook-auth/src/main/java/com/jacob/micro/auth/controller/TestController.java
+++ webook-auth/src/main/java/com/jacob/micro/auth/controller/TestController.java
@@ -1,9 +1,10 @@
 package com.jacob.micro.auth.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.jacob.micro.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.jacob.micro.framework.common.response.Response;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -22,12 +23,27 @@ public class TestController {
         return Response.success("hello, world");
     }
 
-    @GetMapping("/test2")
+    @PostMapping("/test2")
     @ApiOperationLog(description = "测试接口2")
-    public Response<User> test2() {
-        return Response.success(User.builder()
-                .nickName("Jacob")
-                .createTime(LocalDateTime.now())
-                .build());
+    public Response<User> test2(@RequestBody @Validated User user) {
+        int i = 1 / 0;
+        return Response.success(user);
+    }
+
+    // 测试登录，浏览器访问，http://localhost:8080/user/doLogin?username=zhang&password=123456
+    @RequestMapping("/user/doLogin")
+    public String doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if ("jiang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            return "登陆成功";
+        }
+        return "登陆失败";
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:8080/user/isLogin
+    @RequestMapping("/user/isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
     }
 }
