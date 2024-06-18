d0994b003768c2f6b4599d0fe468fc996ac76e17
jdw-art <jdw17733356203@icloud.com>
2024-06-18 11:17:59 +0800

    feat(webook-auth):Role global constant class

---
+++ webook-auth/src/main/java/com/jacob/micro/auth/constant/RoleConstants.java
@@ -0,0 +1,15 @@
+package com.jacob.micro.auth.constant;
+
+/**
+ * @Author: Jacob
+ * @Description: 角色全局常量类
+ * @Date: 2024/6/17 16:56
+ * @Version: 1.0
+ */
+public class RoleConstants {
+
+    /**
+     * 普通用户的角色ID
+     */
+    public static final Long COMMON_USER_ROLE_ID = 1L;
+}
