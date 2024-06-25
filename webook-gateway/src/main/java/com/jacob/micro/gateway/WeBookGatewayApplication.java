15317c2b783ba1900f6cac84a614132c6a9121bf
jdw-art <jdw17733356203@icloud.com>
2024-06-25 14:06:06 +0800

    build(webook-gateway):webook gateway startup class

---
+++ webook-gateway/src/main/java/com/jacob/micro/gateway/WeBookGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.jacob.micro.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Jacob
+ * @Description: webook网关启动类
+ * @Date: 2024/6/25 13:57
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class WeBookGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeBookGatewayApplication.class, args);
+    }
+}
