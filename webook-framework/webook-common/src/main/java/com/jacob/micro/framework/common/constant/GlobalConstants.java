587ff04b62ccb0a87e8212873b03ee117cf089e0
jdw-art <jdw17733356203@icloud.com>
2024-06-26 20:32:06 +0800

    feat(webook-common):Global Constant Class

---
+++ webook-framework/webook-common/src/main/java/com/jacob/micro/framework/common/constant/GlobalConstants.java
@@ -0,0 +1,15 @@
+package com.jacob.micro.framework.common.constant;
+
+/**
+ * @Author: Jacob
+ * @Description: 全局常量类
+ * @Date: 2024/6/26 20:08
+ * @Version: 1.0
+ */
+public interface GlobalConstants {
+
+    /**
+     * 用户 ID
+     */
+    String USER_ID = "userId";
+}
