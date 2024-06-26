b71dcc46b7a55b2cb1124c1f2b4d2d162bd53304
jdw-art <jdw17733356203@icloud.com>
2024-06-26 20:34:14 +0800

    fix(webook-auth):Modify the logout interface

--- webook-auth/src/main/java/com/jacob/micro/auth/controller/UserController.java
+++ webook-auth/src/main/java/com/jacob/micro/auth/controller/UserController.java
@@ -7,10 +7,7 @@ import com.jacob.micro.framework.common.response.Response;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author: Jacob
@@ -35,7 +32,7 @@ public class UserController {
     @PostMapping("/logout")
     @ApiOperationLog(description = "账号登出")
     public Response<?> logout() {
-        // TODO:账号登出
-        return Response.success();
+
+        return userService.logout();
     }
 }
