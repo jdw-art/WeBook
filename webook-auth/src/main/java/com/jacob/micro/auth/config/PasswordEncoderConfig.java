96cb42438ecab4eaa4f8406112785c86670dc9a5
jdw-art <jdw17733356203@icloud.com>
2024-06-30 17:08:37 +0800

    feat(webook-auth):Password encryption configuration class

---
+++ webook-auth/src/main/java/com/jacob/micro/auth/config/PasswordEncoderConfig.java
@@ -0,0 +1,27 @@
+package com.jacob.micro.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: Jacob
+ * @Description: 密码加密配置类
+ * @Date: 2024/6/30 16:34
+ * @Version: 1.0
+ */
+@Component
+public class PasswordEncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // BCrypt 是一种安全且适合密码存储的哈希算法，它在进行哈希时会自动加入“盐”，增加密码的安全性。
+        return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("jdw112233"));
+    }
+}
