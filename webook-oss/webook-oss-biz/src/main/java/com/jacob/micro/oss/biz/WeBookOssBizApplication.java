cba723c4890aff43a3a0922c4cd5ee4e5a6cc2e1
jdw-art <jdw17733356203@icloud.com>
2024-07-01 21:48:00 +0800

    feat(webook):webook-oss Startup Class

---
+++ webook-oss/webook-oss-biz/src/main/java/com/jacob/micro/oss/biz/WeBookOssBizApplication.java
@@ -0,0 +1,17 @@
+package com.jacob.micro.oss.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Jacob
+ * @Description: OSS 服务启动类
+ * @Date: 2024/7/1 21:44
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class WeBookOssBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeBookOssBizApplication.class, args);
+    }
+}
