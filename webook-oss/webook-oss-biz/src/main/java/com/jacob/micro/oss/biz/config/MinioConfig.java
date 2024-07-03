9b6d122c988dba540af107bfd31e127476fcb04d
jdw-art <jdw17733356203@icloud.com>
2024-07-03 20:43:39 +0800

    feat(webook-oss):Client Configuration Class

---
+++ webook-oss/webook-oss-biz/src/main/java/com/jacob/micro/oss/biz/config/MinioConfig.java
@@ -0,0 +1,28 @@
+package com.jacob.micro.oss.biz.config;
+
+import io.minio.MinioClient;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: Jacob
+ * @Description: minio 客户端配置类
+ * @Date: 2024/7/2 21:16
+ * @Version: 1.0
+ */
+@Configuration
+public class MinioConfig {
+
+    @Resource
+    private MinioProperties minioProperties;
+
+    @Bean
+    public MinioClient minioClient() {
+        // 构建 Minio 客户端
+        return MinioClient.builder()
+                .endpoint(minioProperties.getEndpoint())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+    }
+}
