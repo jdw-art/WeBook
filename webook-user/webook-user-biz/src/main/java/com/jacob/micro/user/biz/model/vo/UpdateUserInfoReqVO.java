a6dd4b48eda29ba77bebef06f6c2a3725ebaca17
jdw-art <jdw17733356203@icloud.com>
2024-07-09 21:36:38 +0800

    feat(webook-user):Modify user information such as parameter entity class

---
+++ webook-user/webook-user-biz/src/main/java/com/jacob/micro/user/biz/model/vo/UpdateUserInfoReqVO.java
@@ -0,0 +1,56 @@
+package com.jacob.micro.user.biz.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+
+/**
+ * @Author: Jacob
+ * @Description: 修改用户信息如参数实体类
+ * @Date: 2024/7/7 17:50
+ * @Version: 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateUserInfoReqVO {
+    /**
+     * 头像
+     */
+    private MultipartFile avatar;
+
+    /**
+     * 昵称
+     */
+    private String nickname;
+
+    /**
+     * 小哈书 ID
+     */
+    private String xiaohashuId;
+
+    /**
+     * 性别
+     */
+    private Integer sex;
+
+    /**
+     * 生日
+     */
+    private LocalDate birthday;
+
+    /**
+     * 个人介绍
+     */
+    private String introduction;
+
+    /**
+     * 背景图
+     */
+    private MultipartFile backgroundImg;
+}
