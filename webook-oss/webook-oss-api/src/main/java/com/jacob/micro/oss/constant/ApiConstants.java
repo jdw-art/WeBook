2880503ed54b3401651827b0ec866e8c5f50b069
jacob <jdw17733356203@icloud.com>
2024-07-14 17:10:51 +0800

    feat(webook-oss):OSS service interface constant class

---
+++ webook-oss/webook-oss-api/src/main/java/com/jacob/micro/oss/constant/ApiConstants.java
@@ -0,0 +1,14 @@
+package com.jacob.micro.oss.constant;
+
+/**
+ * @Author: Jacob
+ * @Description: OSS 服务接口常量类
+ * @Date: 2024-07-14 16:45
+ * @Version: 1.0
+ */
+public interface ApiConstants {
+    /**
+     * 服务名称
+     */
+    String SERVICE_NAME = "webook-oss";
+}
