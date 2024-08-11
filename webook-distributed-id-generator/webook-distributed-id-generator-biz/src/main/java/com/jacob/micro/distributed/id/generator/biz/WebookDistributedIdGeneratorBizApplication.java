98090e22150ca830c7a218e14b706d2d7e3ae312
jdw-art <jdw17733356203@icloud.com>
2024-08-11 21:02:01 +0800

    feat(webook-distributed-id):ID generator springboot starter class

---
+++ webook-distributed-id-generator/webook-distributed-id-generator-biz/src/main/java/com/jacob/micro/distributed/id/generator/biz/WebookDistributedIdGeneratorBizApplication.java
@@ -0,0 +1,17 @@
+package com.jacob.micro.distributed.id.generator.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Jacob
+ * @Description: 分布式ID服务启动类
+ * @Date: 2024/8/11 17:49
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class WebookDistributedIdGeneratorBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebookDistributedIdGeneratorBizApplication.class, args);
+    }
+}
