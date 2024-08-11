08f2a639103778bfbda03df756d7ce5f6d0b627e
jdw-art <jdw17733356203@icloud.com>
2024-08-11 20:59:40 +0800

    feat(webook-distributed-id):constant class

---
+++ webook-distributed-id-generator/webook-distributed-id-generator-api/src/main/java/com/jacob/micro/distributed/id/generator/constant/ApiConstants.java
@@ -0,0 +1,16 @@
+package com.jacob.micro.distributed.id.generator.constant;
+
+/**
+ * @Author: Jacob
+ * @Description: 常量
+ * @Date: 2024/8/11 20:32
+ * @Version: 1.0
+ */
+public interface ApiConstants {
+
+    /**
+     * 服务名称
+     */
+    String SERVICE_NAME = "webook-distributed-id-generator";
+
+}
