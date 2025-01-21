34119001d82d94f9896788148248ae19c9070421
jdw-art <jdw17733356203@icloud.com>
2025-01-21 20:01:42 +0800

    fix(webook-data-align):webook-data-align module build

---
+++ webook-data-align/src/main/java/com/jacob/micro/data/align/domain/WebookDataAlignApplication.java
@@ -0,0 +1,19 @@
+package com.jacob.micro.data.align.domain;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Jacob
+ * @Description: TODO
+ * @Date: 2025/1/21 19:53
+ * @Version: 1.0
+ */
+@SpringBootApplication
+@MapperScan("com.jacob.micro.data.align.domain.mapper")
+public class WebookDataAlignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebookDataAlignApplication.class, args);
+    }
+}
